2d7b0dc30aa7f585f327f4132e4d170ed37fc701
Daniel Dupont <danieldd.leinad@gmail.com>
2016-06-28 16:26:01 +0200

    modif lien video

--- Views/top.php
+++ Views/top.php
@@ -2,7 +2,7 @@
     foreach ($data as $item) {
 ?>
     <h3>id : <?= $item->id ?></h3><br/>
-    <video src="<?= $item->file_url ?>" width="400" autoplay></video><br/>
+    <a href="index.php?route=post&id=<?= $item->id ?>"><video src="<?= $item->file_url ?>" width="400"></video></a><br/>
     <p>difference : <?= $item->difference_like ?></p><br/>
 <?php
     }
