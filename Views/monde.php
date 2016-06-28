5a8aad48ce0cbe3eddf83f78a005f31b5e43d68f
Daniel Dupont <danieldd.leinad@gmail.com>
2016-06-28 22:10:46 +0200

    register and login version 1 (no session_start)

--- Views/monde.php
+++
@@ -1,9 +0,0 @@
-<?php
-    foreach ($users as $item) {
-?>
-    <h2><?= $item->pseudo ?></h2>
-    <a href="index.php?route=profil&num=<?= $item->id ?>">Profil</a>
-    <a href="index.php?route=micro&num=<?= $item->id ?>">Chanter</a>
-<?php
-    }
-?>
