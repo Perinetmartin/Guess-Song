3179dddabc3929be4de1c0651b0d8cd8773db8e9
Daniel Dupont <danieldd.leinad@gmail.com>
2016-06-30 11:15:03 +0200

    inte version 0.0.1

--- Views/header/header.php
+++ Views/header/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Guess Song</title>
-    <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Guess What</title>
+        <link rel="stylesheet" href="assets/css/reset.css">
+        <link rel="stylesheet" href="assets/css/css-bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/css-pages/styles.css">
+    </head>
+    <body>
